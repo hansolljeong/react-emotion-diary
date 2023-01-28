c27f43552ae2cca69e3e21ac821e95da59efbc97
Hansol <losnah24@gmail.com>
2023-01-28 23:19:40 +0900

    헤더 props로 넘김. 잘 나오는지 테스트

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import "./App.css";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
+import MyHeader from "./pages/MyHeader";
+import MyButton from "./pages/MyButton";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"Test Header"}
+          leftChild={<MyButton text={"left"} />}
+          rightChild={<MyButton text={"right"} type={"positive"} />}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />

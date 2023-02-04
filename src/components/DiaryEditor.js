115e744da647dd7cd53fdde27bc81fdc8322c086
Hansol <losnah24@gmail.com>
2023-02-04 22:00:16 +0900

    오늘의 감정 선택 부분 완성, 여전히 이미지는 안불러와짐

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,18 +1,51 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
-import { useNavigate } from "react-router-dom";
+import EmotionItem from "./EmotionItem";
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
+export const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "very good",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "good",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "so so",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "bad",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "terrible",
+  },
+];
 
 export const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
 };
 
 const DiaryEditor = ({ isEdit, originData }) => {
+  const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
 
   const navigate = useNavigate();
 
@@ -39,6 +72,19 @@ const DiaryEditor = ({ isEdit, originData }) => {
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                // isSelected={it.emotion_id === emotion}
+              />
+            ))}
           </div>
         </section>
       </div>
